e9e3bc90ab9fb1d2c0531ae6ef4523f74998b9f0
Olga Kulik <kulik_olla@mail.ru>
2020-11-06 21:01:41 +0300

    music app

---
+++ src/store.js
@@ -0,0 +1,16 @@
+export default {
+  songs: [
+    {
+      title: "Sea",
+      artist: "Unknow Artist",
+      src: require("./assets/Sea.mp3"),
+      image: require("./assets/images/sea.jpg"),
+    },
+    {
+      title: "Misterio Interlude",
+      artist: "Unknow Artist",
+      src: require("./assets/Misterio Interlude.mp3"),
+      image: require("./assets/images/guitar.png"),
+    },
+  ],
+};
